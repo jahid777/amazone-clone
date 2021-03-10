0648e2d478148343beea7f35ca0b354763eb71b1
md jahidul islam <za0162hid@gmail.com>
2021-03-10 22:14:58 +0600

    frist commit

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 const Header = () => {
+  const [{basket, user}] = useStateValue();
   return (
     <div className="header">
-      <img
+     <Link to="/">
+     <img
         className="header__logo"
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt=""
       />
+     </Link>
 
       <div className="header__search">
         <input type="text" className="header__searchInput" />
@@ -44,10 +49,12 @@ const Header = () => {
             </span>
         </div>
              
-             <div className="header__optionBasket">
+            <Link to="Checkout">
+            <div className="header__optionBasket">
                 <ShoppingBasketIcon/>
-                <span className="header__optionTwo header__basketCount">0</span>
+                <span className="header__optionTwo header__basketCount">{basket?.length}</span>
              </div>
+            </Link>
 
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
-const Product = ({title, image, price, rating}) => {
+const Product = ({id,title, image, price, rating}) => {
+    const [{basket}, dispatch] = useStateValue();
+    const addToBasket = () => {
+        // dispatch item into the data layer
+        dispatch({
+            type:'ADD_TO_BASKET',
+            item:{
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            }
+        })
+    };
     return (
         <div className="product">
             <div className="product__info">
@@ -17,7 +32,7 @@ const Product = ({title, image, price, rating}) => {
                 </div>                 
             </div>
              <img src={image}  alt=""/>
-             <button>add to basket</button>
+             <button onClick={addToBasket}> Add to Basket </button>
         </div>
     );
 };
